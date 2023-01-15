243c9c717b17393949ec6ee3cd9197bad9ab5d26
Gerard Hernandez <ger.almenara@gmail.com>
2023-01-15 23:04:42 +0000

    API + Models

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM pytorch/pytorch:1.12.1-cuda11.3-cudnn8-runtime
+
+WORKDIR /bot
+COPY . /bot/
+
+RUN pip install -r requirements.txt
+
+RUN apt update && apt install -y git curl && pip install --upgrade git+https://github.com/huggingface/diffusers.git
+
+EXPOSE 9568
+
+ENTRYPOINT ["uvicorn" ,"api:app", "--port", "9568", "--host", "0.0.0.0"]
---
+++ api.py
@@ -0,0 +1,56 @@
+from io import BytesIO
+from fastapi import FastAPI, File, UploadFile
+import torch
+from diffusers import StableDiffusionPipeline
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import StreamingResponse
+import cv2
+import numpy as np
+from PIL import Image
+
+app = FastAPI()
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+
+@app.get("/")
+async def root():
+    return {"response": "Hello, welcome to this stable diffusion API!"}
+
+
+@app.get("/generate")
+async def generate(prompt: str, inference_steps: int = 50, guideance_scale: float = 7.5, negative_prompt: str = None, height: int = 568, width: int = 568):
+    image = pipe(prompt, num_inference_steps=inference_steps, guidance_scale=guideance_scale, negative_prompt=negative_prompt,
+                 height=height, width=width).images[0]
+
+    hires_img = BytesIO()
+    image.save(hires_img, "PNG")
+    hires_img.seek(0)
+
+    return StreamingResponse(content=hires_img, media_type="image/png")
+
+
+@app.post("/upscale")
+def image_filter(image: UploadFile = File(...), upscaler: str = "edsr", scale: int = 2):
+
+    upscaler_f_name = upscaler + "_" + str(scale) + ".pb"
+
+    original_image = Image.open(image.file)
+
+    lowres_img = np.array(original_image)
+
+    super_res = cv2.dnn_superres.DnnSuperResImpl_create()
+
+    super_res.readModel(upscaler_f_name)
+    super_res.setModel(upscaler, scale)
+    espcn_image = super_res.upsample(lowres_img)
+
+    lowres_img = Image.fromarray(espcn_image)
+
+    hires_img = BytesIO()
+    lowres_img.save(hires_img, "PNG")
+    hires_img.seek(0)
+
+    return StreamingResponse(hires_img, media_type="image/png")
